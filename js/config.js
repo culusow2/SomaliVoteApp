e3b5115d5b0ab3e4feaeecf0ca46d9d41073e2f2
culusow2 <bakaribra4me@gmail.com>
2025-04-25 17:00:35 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,11 @@
-const firebaseConfig = {
+// Firebase configuration
+export const firebaseConfig = {
   apiKey: "AIzaSyC7c6A2Tt7uqOf7ZQWcTKsh3DOXzDkMwAE",
   authDomain: "somalivoteapp.firebaseapp.com",
   databaseURL: "https://somalivoteapp-default-rtdb.firebaseio.com",
   projectId: "somalivoteapp",
-  storageBucket: "somalivoteapp.firebasestorage.app",
+  storageBucket: "somalivoteapp.appspot.com",
   messagingSenderId: "957146346027",
   appId: "1:957146346027:web:5b79ae4f2f43f694c0b06e",
   measurementId: "G-VG10Q15161"
 };
-
-export default firebaseConfig;
