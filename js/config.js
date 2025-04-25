8cafd42198aad71b93cdb521b9a0e524a7fe59de
culusow2 <bakaribra4me@gmail.com>
2025-04-25 15:17:44 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+﻿export const firebaseConfig = {
+    apiKey: "AIzaSyC7c6A2Tt7uqOf7ZQWcTKsh3DOXzDkMwAE",
+    authDomain: "somalivoteapp.firebaseapp.com",
+    databaseURL: "https://somalivoteapp-default-rtdb.firebaseio.com",
+    projectId: "somalivoteapp",
+    storageBucket: "somalivoteapp.firebasestorage.app",
+    messagingSenderId: "957146346027",
+    appId: "1:957146346027:web:5b79ae4f2f43f694c0b06e",
+    measurementId: "G-VG10Q15161"
+};
